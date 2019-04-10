b0e43dc53c0e60d175933fa380853d9bf5871483
Renan Toniolo Rocha <renantoniolo@gmail.com>
2019-04-10 16:57:30 -0300

    Ajustes xaml do projeto.

--- TesteUnitario/App.xaml.cs
+++ TesteUnitario/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TesteUnitario.View;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace TesteUnitario
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new CadastroCliente();
         }
 
         protected override void OnStart()
